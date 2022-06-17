dbb02beb199f26812ec572a95bca5d65ce0ac13f
fAEDKAN <sfdk.94@hotmail.com>
2022-06-16 22:15:09 -0300

    buscando error

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const app = express();
 const port = 3030;
 
 /* recursos estáticos */
-app.use(express.static('./public/img'));
+app.use(express.static('public'));
 
 /* rutas */
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+
 app.get('/babbage', (req, res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')));
 app.get('/berners-lee', (req, res) => res.sendFile(path.join(__dirname, 'views', 'berners-lee.html')));
 app.get('/clarke', (req, res) => res.sendFile(path.join(__dirname, 'views', 'clarke.html')));
